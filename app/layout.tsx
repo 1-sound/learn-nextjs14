5a354f5d72d937e613b7105986e9788309a809a3
hansol.seo <seohansol040418@gmail.com>
2024-04-09 22:29:49 +0900

    public

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,13 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          href="/assets/icons/icon-384x384.png"
+        ></link>
+      </head>
       <body>
         <Navigation />
         {children}
